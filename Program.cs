249b3888dea4ae6eb871f059f0fb4f55323c9924
Milan Humagain <10011027@BCS.NET.NZ>
2017-09-12 15:48:35 +1200

    Even numbers practice

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace array_evenNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var numbers = new int[] {2, 4, 5, 78, 45, 99, 88, 100, 203, 111};
+          var evenNumbers = new List<int>();
+
+          foreach(var x in numbers) {
+          if(x % 2 != 0){
+          evenNumbers.Add(x);
+
+          }
+        }
+
+        Console.WriteLine("the given array in ascendig order");
+        Array.Sort(numbers);
+
+         foreach(var x in numbers) {
+         Console.WriteLine(x);
+         }
+             
+    }
+}
+    }
